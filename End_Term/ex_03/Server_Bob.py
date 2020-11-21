aa6c9d7689899d61b88c7bff1df70089c2684f02
차지원 <chajiwon100785@gmail.com>
2020-11-22 03:19:06 +0900

    프로젝트 3번 Diffe-Hellman 알고리즘 적용

--- End_Term/ex_03/Server_Bob.py
+++ End_Term/ex_03/Server_Bob.py
@@ -1,8 +1,27 @@
+# diffe-hellman 알고리즘을 통해 비밀키를 만들어 정보를 주고받는다.
 # Client에게 제곱값을 반환시켜준다.
 # 사용자 인증이 가능하다.
 # 대칭키 암호화 방식으로 DES 방식을 채택한다.
 from socket import *
 from Crypto.Cipher import DES
+
+def diffe_key(client_data):
+    # 공개키
+    g = 221185 
+    p = 1340
+
+    # 임의의 정수 b
+    b = 12
+    B = g**b % p
+
+    # 앨리스에게 전송
+    client_data.send(B.to_bytes(8,'big'))
+
+    # 앨리스에게서 A를 전송받는다.
+    A_bytes = client_data.recv(1024)
+    A = int.from_bytes(A_bytes, 'big')
+    s = A**b % p
+    return s.to_bytes(8,'big')
 
 # TCP 연결, 18000포트
 port_number = 18000
@@ -10,9 +29,6 @@ s = socket(AF_INET,SOCK_STREAM)
 s.bind(('127.0.0.1',port_number))
 s.listen(1)
 
-# 대칭키 암호화
-key = 'qwerasdf'.encode()
-des = DES.new(key, DES.MODE_ECB)
 
 while True:
     trust = False
@@ -21,6 +37,10 @@ while True:
     clientData,addr = s.accept()
     print('다음 클라이언트가 연결됩니다. ',addr)
     
+    # 대칭키 암호화
+    key = diffe_key(clientData)
+    des = DES.new(key, DES.MODE_ECB)
+
     # 암호화된 사용자 인증
     user_id = clientData.recv(1024)
     user_pw = clientData.recv(1024)
@@ -28,7 +48,6 @@ while True:
     decoded_id = des.decrypt(user_id).decode().rstrip()
     decoded_pass = des.decrypt(user_pw).decode().rstrip()
 
-    print(decoded_id, decoded_pass)
     with open('pw.txt',"r") as f:
         pw_id = f.readline().rstrip()
         pw_pass = f.readline().rstrip()
@@ -41,9 +60,10 @@ while True:
 
     # 제곱값을 반환하는 서버
     while trust:
-        data = clientData.recv(1024).decode()
+        data = clientData.recv(1024)
         if not data:
             break
-        
-        pow_number = data + "의 제곱값은 " + str(int(data)*int(data)) + "입니다."
+        decoded_data = des.decrypt(data).decode().rstrip()
+        pow_number = decoded_data + "의 제곱값은 " \
+                    + str(int(decoded_data)*int(decoded_data)) + "입니다."
         clientData.sendall(pow_number.encode())
